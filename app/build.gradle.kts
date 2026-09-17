91c47f42857d92e7c59666ff3277d3b5c530b977
Kevin-AC <kevin.argumedo@gmail.com>
2026-09-16 21:12:43 -0500

    "Configuracion de permisos en Manifest" "Configuracion de Dependencias"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,11 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.media3.exoplayer)
 }
